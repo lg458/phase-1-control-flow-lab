c70b40d133855b7149f07e4341320d8bc2f56acb
Laura Gonzalez <lg458@cornell.edu>
2022-06-27 19:02:58 -0700

    commit tree f79636e4b516e5677c16744e0ceb558acb64737d

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(dist){
+  if (dist <= 400) { 
+    return "This one is on me!"; 
+  } 
+  else if (dist > 400 && dist <= 2000) { 
+    return "That will be twenty bucks.";  
+  }
+  else if (dist > 2000 && dist < 2500) {
+     return "I will gladly take your thirty bucks.";
+  } 
+  else { 
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === "NYC" ? "Ok, sounds good." : "No go."; 
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) { 
+    case ('generous'): 
+      return "Thank you so much."; 
+    case ('not as generous'): 
+        return "Thank you.";
+    case ('thanks for everything'): 
+      return "Bye.";
+  }
 }
